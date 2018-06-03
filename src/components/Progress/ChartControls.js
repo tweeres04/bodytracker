8a3ba59e3e7bc084a888d9102703e7b53ffc2ac6
Tyler Weeres <tweeres04@gmail.com>
2018-06-03 14:29:57 -0700

    DRY up ChartControls

--- src/components/Progress/ChartControls.js
+++ src/components/Progress/ChartControls.js
@@ -1,6 +1,57 @@
 import React, { Component, Fragment } from 'react';
 import Flatpickr from 'react-flatpickr';
 
+const datepickerCommonProps = {
+	className: 'input',
+	options: {
+		maxDate: 'today'
+	}
+};
+
+function StartDatepicker({ start, onDateChange }) {
+	return (
+		<div className="control">
+			<Flatpickr
+				placeholder="Start date"
+				value={start}
+				onChange={([start]) => {
+					onDateChange(start, 'start');
+				}}
+				{...datepickerCommonProps}
+			/>
+		</div>
+	);
+}
+
+function EndDatepicker({ end, onDateChange }) {
+	return (
+		<div className="control">
+			<Flatpickr
+				placeholder="End date"
+				value={end}
+				onChange={([end]) => {
+					onDateChange(end, 'end');
+				}}
+				{...datepickerCommonProps}
+			/>
+		</div>
+	);
+}
+
+function ResetButton({ start, end, resetDateRange }) {
+	return (
+		<div className="control">
+			<button
+				className="button"
+				onClick={resetDateRange}
+				disabled={!start && !end}
+			>
+				Reset
+			</button>
+		</div>
+	);
+}
+
 export default class ChartControls extends Component {
 	state = {
 		start: null,
@@ -11,83 +62,27 @@ export default class ChartControls extends Component {
 		return (
 			<Fragment>
 				<div className="field is-grouped is-hidden-mobile">
-					<div className="control">
-						<Flatpickr
-							className="input"
-							placeholder="Start date"
-							value={start}
-							onChange={([start]) => {
-								this.onDateChange(start, 'start');
-							}}
-							options={{
-								maxDate: 'today'
-							}}
-						/>
-					</div>
-					<div className="control">
-						<Flatpickr
-							className="input"
-							placeholder="End date"
-							value={end}
-							onChange={([end]) => {
-								this.onDateChange(end, 'end');
-							}}
-							options={{
-								maxDate: 'today'
-							}}
-						/>
-					</div>
-					<div className="control">
-						<button
-							className="button"
-							onClick={this.resetDateRange}
-							disabled={!start && !end}
-						>
-							Reset
-						</button>
-					</div>
+					<StartDatepicker onDateChange={this.onDateChange} start={start} />
+					<EndDatepicker onDateChange={this.onDateChange} end={end} />
+					<ResetButton
+						start={start}
+						end={end}
+						resetDateRange={this.resetDateRange}
+					/>
 				</div>
 				<div className="is-hidden-tablet">
 					<div className="field">
-						<div className="control">
-							<Flatpickr
-								className="input"
-								placeholder="Start date"
-								value={start}
-								onChange={([start]) => {
-									this.onDateChange(start, 'start');
-								}}
-								options={{
-									maxDate: 'today'
-								}}
-							/>
-						</div>
+						<StartDatepicker onDateChange={this.onDateChange} start={start} />
 					</div>
 					<div className="field">
-						<div className="control">
-							<Flatpickr
-								className="input"
-								placeholder="End date"
-								value={end}
-								onChange={([end]) => {
-									this.onDateChange(end, 'end');
-								}}
-								options={{
-									maxDate: 'today'
-								}}
-							/>
-						</div>
+						<EndDatepicker onDateChange={this.onDateChange} end={end} />
 					</div>
 					<div className="field">
-						<div className="control">
-							<button
-								className="button"
-								onClick={this.resetDateRange}
-								disabled={!start && !end}
-							>
-								Reset
-							</button>
-						</div>
+						<ResetButton
+							start={start}
+							end={end}
+							resetDateRange={this.resetDateRange}
+						/>
 					</div>
 				</div>
 			</Fragment>
